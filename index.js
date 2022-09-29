878dfee48c438110d3a33284e975283218484fc5
Knox-Phillips <Knox-Phillips@citypolyhs.org>
2022-09-29 19:36:19 +0000

    Bonus question 12

--- index.js
+++ index.js
@@ -82,3 +82,8 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 }
 
 printTime()
+
+function isleapYear(year){
+return (0 == year % 4 && 0 != year % 100 || 0 == year % 400) ? year + ' ' + 'is a leap year' : year + ' ' + 'is not a leap year'
+}
+console.log(isleapYear(2002))
