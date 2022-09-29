81fe3bae7b8723cb7031e2cfa30b36b4c7b23d23
Ambs1023 <amberlambright4@gmail.com>
2022-09-29 18:18:40 +0000

    finished function 3

--- index.js
+++ index.js
@@ -14,3 +14,13 @@ function multiplesOfThree() {
     }
 }
 multiplesOfThree()
+
+function multiplesOfThreeOrFive(){
+    for (let i = 1; i <= 100; i++){
+        if (i % 3 === 0 || i % 5 === 0){
+        console.log (i)
+            
+        }
+    }
+}
+multiplesOfThreeOrFive()
