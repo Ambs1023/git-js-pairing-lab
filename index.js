0488c65eec351f3fd43f475df30d699278429f03
Knox-Phillips <Knox-Phillips@citypolyhs.org>
2022-09-29 18:13:13 +0000

    Solved question 2

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ function fiveToOneHundred(){
     }
 }
 fiveToOneHundred()
+
+function multiplesOfThree() {
+    for (let i = 3; i <= 100; i++) {
+        if(i % 3 === 0) {
+            console.log(i)
+        }
+    }
+}
+multiplesOfThree()
